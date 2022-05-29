bf2edc6df04ed577dd7bc5fcef61902277437848
Oreo <thediamondgamer331@gmail.com>
2022-05-29 23:50:20 +0100

    changes

--- test/auto_update/update.js
+++ test/auto_update/update.js
@@ -1,3 +1,87 @@
-const GitHub = require('get-github-code');
+const fs = require('fs');
+const https = require('https');
 
-GitHub.download('https://github.com/btmc-bot/btmc/blob/main/index.js')
+const url = 'https://api.github.com/repos/btmc-bot/btmc/contents/src';
+
+let todo = [];
+
+const options = {
+    hostname: 'api.github.com',
+    path: '/repos/btmc-bot/btmc/contents/src',
+    method: 'GET',
+    headers: {
+        'User-Agent': 'btmc-bot',
+    }
+}
+
+const req = https.request(options, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    res.on('end', () => {
+        const finalData = JSON.parse(data);
+
+        for (const file of finalData) {
+            todo.push(file);
+            run();
+        }
+    })
+}).end();
+
+async function run() {
+    while (todo.length > 0) {
+        const file = todo.shift();
+
+        if (file.type == 'dir') {
+            const options = {
+                hostname: 'api.github.com',
+                path: '/repos/btmc-bot/btmc/contents/' + file.path,
+                method: 'GET',
+                headers: {
+                    'User-Agent': 'btmc-bot',
+                }
+            }
+
+            _res = await makeReq(options);
+            res = JSON.parse(_res);
+
+            for (const subFile of res) {
+                todo.push(subFile);
+            }
+        }
+
+        if (file.type == 'file') {
+            const options = {
+                hostname: 'raw.githubusercontent.com',
+                path: '/btmc-bot/btmc/main/' + file.path,
+                method: 'GET',
+                headers: {
+                    'User-Agent': 'btmc-bot',
+                }
+            }
+
+            const res = await makeReq(options);
+
+            const path = './' + file.path;
+
+            fs.writeFileSync(path, res);
+        }
+    }
+}
+
+function makeReq(options) {
+    return new Promise(reso => {
+        const req = https.request(options, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                reso(data);
+            })
+        }).end();
+    })
+}
