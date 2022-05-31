2ad69ba5fd6ddac3f7b1244d517f3252ab9a903e
Oreo <thediamondgamer331@gmail.com>
2022-05-31 02:24:17 +0100

    create interaction handler

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,3 +1,11 @@
 module.exports = function (client, interaction) {
-  //
+  if (interaction.isCommand()) commandInteraction(client, interaction);
+}
+
+function commandInteraction(client, interaction) {
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  command.execute_s(client, interaction);
 }
