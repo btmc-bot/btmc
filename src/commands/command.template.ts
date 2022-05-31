2862aae2aa6de089a2b5dc2371c552ed7ec82e74
Oreo <thediamondgamer331@gmail.com>
2022-05-31 02:24:08 +0100

    template change

--- src/commands/command.template.ts
+++ src/commands/command.template.ts
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: "help", // COMMAND NAME
@@ -13,6 +14,6 @@ module.exports = {
         name: 'help',
         description: 'Displays all commands',
     },
-    execute_m(message, args, client) { }, // THE EXECUTION THROUGH MESSAGE
-    execute_s(interaction, args, client) { } // THE EXECUTION THROUGH SLASH COMMAND
+    execute_m(client, message, args) { }, // THE EXECUTION THROUGH MESSAGE
+    execute_s(client, interaction) { } // THE EXECUTION THROUGH SLASH COMMAND
 }
