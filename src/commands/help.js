1ec4b933cb99326c392a2f5a437882688188f77c
Oreo <thediamondgamer331@gmail.com>
2022-05-31 02:24:36 +0100

    Finish help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,63 +1,106 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const catLink = {
+    sys: {
+        "cat_info": "info",
+        "cat_moderation": "moderation",
+        "cat_fun": "fun",
+        "cat_music": "music",
+        "cat_other": "other",
+        "cat_all": "all"
+    },
+    text: {
+        "info": "Information",
+        "moderation": "Moderation",
+        "fun": "Fun",
+        "music": "Music",
+        "other": "Other",
+        "all": "All"
+    }
+}
 
 module.exports = {
     name: "help",
-    description: "Displays all commands",
+    description: "Displays information about BTMC",
     help_menu: {
         display: true,
         devlock: false,
         category: "info"
     },
-    data: {
-        name: 'help',
-        description: 'Displays all commands',
-    },
-    execute_m(message, args, client) { },
-    execute_s(interaction, args, client) {
-        const __commands = client.commands.array();
-
-        const _commands = __commands.filter(command => command.help_menu.display);
-
-        let commands = {
-            info: [],
-            fun: [],
-            mod: [],
-            dev: [],
-            music: [],
-            other: []
-        }
-        commands = packToCat(_commands, commands);
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Displays information about BTMC')
+        .addStringOption(option =>
+            option.setName('category')
+                .setDescription('The help command category you want to display')
+                .setRequired(false)
+                .addChoices(
+                    { name: 'Information', value: 'cat_info' },
+                    { name: 'Moderation', value: 'cat_moderation' },
+                    { name: 'Fun', value: 'cat_fun' },
+                    { name: 'Music', value: 'cat_music' },
+                    { name: 'Other', value: 'cat_other' },
+                    { name: 'All', value: 'cat_all' }
+                )
+        )
+    ,
+    execute_m(client, message, args) { },
+    async execute_s(client, interaction) {
+        const category = interaction.options.get('category').value;
 
         const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
-                    .setCustomId('help_displaylocked')
-                    .setLabel('Show locked commands')
+                    .setCustomId('help_showdev')
+                    .setLabel('Show Hidden Commands')
                     .setStyle('DANGER')
             )
         //
 
+        if (!category) {
+            const embed = new MessageEmbed()
+                .setTitle('BTMC Help')
+                .setDescription('BTMC is a Multi Purpose discord bot made for any size of server')
+                .addFields(
+                    { name: 'Help Categories', value: 'Our help categories are Info, Moderation, Fun, Music, Other or you can get all commands using "all"', inline: true },
+                    { name: 'Support', value: 'If you need help with BTMC, join our support server [here](https://discord.gg/fPHQeUkrkd)', inline: true },
+                    { name: 'Website', value: 'BTMC\'s main website is [btmc.dev](https://btmc.dev) however we also have a [blog](https://blog.btmc.dev) site', inline: true }
+                )
+                .setColor('#32a89d');
+
+            return await interaction.reply({ embeds: [embed], components: [row], ephemeral: true });
+        }
+
+        const hCategory = catLink.sys[category];
+        const hText = catLink.text[hCategory];
+
+        const commands = client.commands.filter(command => command.help_menu.category === hCategory && command.help_menu.display);
+
         const embed = new MessageEmbed()
-            .setTitle(`${client.user.username} Help Menu`)
-            .setDescription('Here are all of btmcs commands!')
-            .addFields(
-                { name: 'Info', value: commands.info.map(command => `\`${command.name}\`\n> ${command.description}`).join(', ') },
-                { name: 'Fun', value: commands.fun.map(command => `\`${command.name}\`\n> ${command.description}`).join(', ') },
-                { name: 'Moderation', value: commands.mod.map(command => `\`${command.name}\`\n> ${command.description}`).join(', ') },
-                { name: 'Music', value: commands.music.map(command => `\`${command.name}\`\n> ${command.description}`).join(', ') },
-                { name: 'Other', value: commands.other.map(command => `\`${command.name}\`\n> ${command.description}`).join(', ') },
-            )
+            .setTitle('BTMC Help (' + hText.toLowerCase() + ')')
+            .setDescription(hText + ' commands')
+            .setColor('#32a89d');
         //
 
-        interaction.reply({ embeds: [embed], components: [row], ephemeral: true })
+        if (category === 'cat_all') {
+            const _commands = client.commands.filter(command => command.help_menu.display);
+
+            _commands.forEach(command => {
+                embed.addField(command.name, `${command.description}\nCategory: ${command.help_menu.category}`, true);
+            });
+
+            return await interaction.reply({ embeds: [embed], ephemeral: true });
+        }
+
+        if (commands.size === 0) {
+            return await interaction.reply({ content: 'There are no commands in this category', ephemeral: true });
+        }
+
+        commands.forEach(command => {
+            embed.addField(command.name, command.description, true);
+        })
+
+        return await interaction.reply({ embeds: [embed], ephemeral: true });
     }
-}
-
-function packToCat(commands, cats) {
-    commands.forEach(command => {
-        if (command.help.devlock) cats['dev'].push(command);
-        else cats[command.help_menu.category].push(command);
-    });
-
-    return cats;
 }
